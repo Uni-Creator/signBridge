e9ed3758cf90135fa944b28afb13ca5873d4397f
Uni-Creator <abhayr24564@gmail.com>
2026-05-17 21:02:34 +0530

    fix: switch gunicorn worker from gevent to eventlet to resolve SSL recursion with pyrebase

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,10 +1,6 @@
 import os
 
-def post_fork(server, worker):
-    from gevent import monkey
-    monkey.patch_all()
-
-worker_class = "gevent"
+worker_class = "eventlet"
 workers = 1
 timeout = 120
 bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
